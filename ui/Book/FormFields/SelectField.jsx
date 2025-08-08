1513bcd590125bb86f8f5d17f8ad6fdf7c82e8ae
sfrunza <frunza.sergiu3@gmail.com>
2025-08-08 16:34:09 -0400

    asd

--- ui/Book/FormFields/SelectField.jsx
+++ ui/Book/FormFields/SelectField.jsx
@@ -1,6 +1,6 @@
-import { useField, useFormikContext } from 'formik';
-import React from 'react';
-import clsx from 'clsx';
+import { useField, useFormikContext } from "formik";
+import React from "react";
+import clsx from "clsx";
 
 export default function SelectField(props) {
   const { label, options, icon } = props;
@@ -13,21 +13,21 @@ export default function SelectField(props) {
 
   function handleChnage(e) {
     const val = e.target.value;
-    const removeToZip = val.localeCompare('Moving') !== 0;
-    const removeDeliveryDate = val.localeCompare('Moving with Storage') !== 0;
+    const removeToZip = val.localeCompare("Moving") !== 0;
+    const removeDeliveryDate = val.localeCompare("Moving with Storage") !== 0;
 
-    if (name === 'service') {
+    if (name === "service") {
       if (removeToZip && removeDeliveryDate) {
-        formikProps.setFieldValue(`deliveryDate`, '');
-        formikProps.setFieldValue(`destinationAddress`, '');
-        formikProps.setFieldValue(`destinationCity`, '');
-        formikProps.setFieldValue(`destinationState`, '');
-        formikProps.setFieldValue(`destinationZip`, '');
-        formikProps.setFieldValue(`destinationFloor`, '');
-        formikProps.setFieldValue(`destinationApt`, '');
+        formikProps.setFieldValue(`deliveryDate`, "");
+        formikProps.setFieldValue(`destinationAddress`, "");
+        formikProps.setFieldValue(`destinationCity`, "");
+        formikProps.setFieldValue(`destinationState`, "");
+        formikProps.setFieldValue(`destinationZip`, "");
+        formikProps.setFieldValue(`destinationFloor`, "");
+        formikProps.setFieldValue(`destinationApt`, "");
       }
       if (removeDeliveryDate) {
-        formikProps.setFieldValue(`deliveryDate`, '');
+        formikProps.setFieldValue(`deliveryDate`, "");
       }
     }
     field.onChange(e);
@@ -37,20 +37,20 @@ export default function SelectField(props) {
     <>
       <label
         htmlFor={field.name}
-        className="block text-sm font-medium leading-6"
+        className="block text-sm leading-6 font-medium"
       >
         {label}
       </label>
       <div className="relative rounded-md">
-        <div className="z-10 pointer-events-none absolute inset-y-0 left-0 mt-1 flex items-center pl-3">
+        <div className="pointer-events-none absolute inset-y-0 left-0 z-10 mt-1 flex items-center pl-3">
           {/* {icon &&
             React.createElement(icon, { width: 12, height: 12, color: '#000' })} */}
           {icon && (
             <props.icon
-              className={clsx('h-4 w-4 stroke-[1.5]', {
-                'text-slate-400': field.value === '',
-                'text-slate-900': isError,
-                'text-slate-900': isSuccess,
+              className={clsx("h-4 w-4 stroke-[1.5]", {
+                "text-slate-400": field.value === "",
+                "text-slate-900": isError,
+                "text-slate-900": isSuccess,
               })}
               aria-hidden="true"
             />
@@ -62,17 +62,17 @@ export default function SelectField(props) {
           value={value}
           onChange={handleChnage}
           className={clsx(
-            'relative mt-1 lock w-full rounded-md border-0 bg-slate-100 font-normal placeholder:text-slate-400 py-2 pl-10 pr-3 text-base focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm',
+            "relative mt-1 h-10 w-full rounded-md border-0 bg-slate-100 py-2 pr-3 pl-10 text-base font-normal placeholder:text-slate-400 focus:border-indigo-500 focus:ring-indigo-500 focus:outline-none sm:text-sm",
             {
-              'text-slate-400': value === '',
-              'shadow-inner !bg-red-50': isError,
-              'shadow-inner !bg-green-50': isSuccess,
+              "text-slate-400": value === "",
+              "!bg-red-50 shadow-inner": isError,
+              "!bg-green-50 shadow-inner": isSuccess,
             },
           )}
         >
           {options.map((o) => {
             return (
-              <option key={o.label} disabled={o.value === ''} value={o.value}>
+              <option key={o.label} disabled={o.value === ""} value={o.value}>
                 {o.label}
               </option>
             );
--- ui/Book/FormFields/ZipField.jsx
+++ ui/Book/FormFields/ZipField.jsx
@@ -1,12 +1,12 @@
-import { useField, useFormikContext } from 'formik';
-import clsx from 'clsx';
-import { jsonCityState } from '../UsCities';
-import toast from 'react-hot-toast';
-import { MapPinIcon } from '@heroicons/react/24/outline';
+import { useField, useFormikContext } from "formik";
+import clsx from "clsx";
+import { jsonCityState } from "../UsCities";
+import toast from "react-hot-toast";
+import { MapPinIcon } from "@heroicons/react/24/outline";
 // import { MapPinIcon } from '@heroicons/react/20/solid';
 
 const onlyNumbers = (e) => {
-  e.target.value = e.target.value.replace(/[^0-9]/g, '');
+  e.target.value = e.target.value.replace(/[^0-9]/g, "");
 };
 
 const findCity = (zip) => {
@@ -32,7 +32,7 @@ export default function ZipField(props) {
   function notify(value) {
     let obj = findCity(value);
     if (obj) {
-      if (name === 'zip') {
+      if (name === "zip") {
         formikProps.setFieldValue(`city`, obj.c);
         formikProps.setFieldValue(`state`, obj.s);
         toast(
@@ -44,7 +44,7 @@ export default function ZipField(props) {
             </span>
           ),
           {
-            icon: '🚚',
+            icon: "🚚",
           },
         );
       } else {
@@ -53,36 +53,36 @@ export default function ZipField(props) {
         toast(
           () => (
             <span>
-              Moving {name === 'origin' ? 'from' : 'to'}{' '}
+              Moving {name === "origin" ? "from" : "to"}{" "}
               <b>
                 {obj.c}, {obj.s}
               </b>
             </span>
           ),
           {
-            icon: '🚚',
+            icon: "🚚",
           },
         );
       }
     } else {
-      toast.error('Invalid zip code');
+      toast.error("Invalid zip code");
     }
   }
   return (
     <>
       <label
         htmlFor={field.name}
-        className="block text-sm font-medium leading-6"
+        className="block text-sm leading-6 font-medium"
       >
         {label}
       </label>
       <div className="relative rounded-md">
-        <div className="z-10 pointer-events-none absolute inset-y-0 left-0 mt-1 flex items-center pl-3">
+        <div className="pointer-events-none absolute inset-y-0 left-0 z-10 mt-1 flex items-center pl-3">
           <MapPinIcon
-            className={clsx('h-5 w-5', {
-              'text-slate-400': field.value === '',
-              'text-slate-900': isError,
-              'text-slate-900': isSuccess,
+            className={clsx("h-5 w-5", {
+              "text-slate-400": field.value === "",
+              "text-slate-900": isError,
+              "text-slate-900": isSuccess,
             })}
             aria-hidden="true"
           />
@@ -96,11 +96,11 @@ export default function ZipField(props) {
           pattern="[0-9]*"
           maxLength="5"
           className={clsx(
-            'relative mt-1 lock w-full rounded-md border-0 bg-slate-100 font-normal placeholder:text-slate-400 py-2 pl-10 pr-3 text-base focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm',
+            "relative mt-1 w-full rounded-md border-0 bg-slate-100 py-2 pr-3 pl-10 text-base font-normal placeholder:text-slate-400 focus:border-indigo-500 focus:ring-indigo-500 focus:outline-none sm:text-sm",
             {
-              'text-slate-400': field.value === '',
-              'shadow-inner !bg-red-50': isError,
-              'shadow-inner !bg-green-50': isSuccess,
+              "text-slate-400": field.value === "",
+              "!bg-red-50 shadow-inner": isError,
+              "!bg-green-50 shadow-inner": isSuccess,
             },
           )}
           onInput={onlyNumbers}
